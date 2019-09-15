6cf971418c2e56c454c7ac9798d711e22b3ec74d
Marcus Behel <marcus.behel@mindspring.com>
2019-09-14 23:36:48 -0400

    Add action handlers for buttons

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -1,20 +1,50 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Marcus Behel on 9/14/19.
-//  Copyright © 2019 Marcus Behel. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    /// Button actions
+    @IBAction func clearPressed(_ sender: Any) {
+        numberField.text = ""
+    }
+    
+    @IBAction func dividePressed(_ sender: Any) {
+    }
+    
+    @IBAction func multiplyPressed(_ sender: Any) {
+    }
+    
+    @IBAction func subtractPressed(_ sender: Any) {
+    }
+    
+    @IBAction func addPressed(_ sender: Any) {
+    }
+    
+    @IBAction func equalsPressed(_ sender: Any) {
+    }
+    
+    @IBAction func decimalPressed(_ sender: Any) {
+        // Make sure text is not nil
+        if(numberField.text == nil){
+            numberField.text = ""
+        }
+        // If there is no text entered yet
+        if(!numberField.text!.contains(".")){
+            numberField.text = numberField.text! + "."
+        }
+    }
+    
+    // This handles any number button
+    @IBAction func numberPressed(_ sender: Any) {
+        let button = sender as! UIButton
+        numberField.text = numberField.text! + button.titleLabel!.text!
+    }
+    
 }
 
