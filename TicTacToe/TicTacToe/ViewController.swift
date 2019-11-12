7e251afd594c5f26e46beae47fe870fe7265d7eb
Marcus Behel <marcus.behel@mindspring.com>
2019-11-12 15:34:37 -0500

    Added initial tic tac toe UI

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Marcus Behel on 11/12/19.
+//  Copyright © 2019 Marcus Behel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
